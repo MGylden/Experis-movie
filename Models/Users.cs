5e1f7eef49d5528c85c6231fe6d7cbf811408e7a
MGylden <mathias@gylden.net>
2022-05-21 02:53:55 +0200

    --

--- Models/Users.cs
+++ Models/Users.cs
@@ -17,12 +17,11 @@ namespace Experis_movie
     {
         public Users(int id, string name, string viewed, string purchased)
         {
-                                
             this.UserId = id;
             this.UserName = name;
             this.UserViewed = viewed;
             this.UserPurchased = purchased;
-
+            this.UserPurchasedSplit = SplitUserPurchased();
         }
 
         [Index(0)]
@@ -33,9 +32,17 @@ namespace Experis_movie
         public string UserViewed { get; set; }
         [Index(3)]
         public string UserPurchased { get; set; }
+        public List<string> UserPurchasedSplit { get; set; }
 
+        public List<string> SplitUserPurchased()
+        {
+            List<string> split = new List<string>();
+            string[] stringArray = this.UserPurchased.Split(';');
 
-
+            foreach (var item in stringArray)
+                split.Add(item);
+            return split;
+        }
        
     }
 }
---
+++ Operations/MostPopularMovies.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using CsvHelper.TypeConversion;
+using System.Globalization;
+using System.Collections;
+
+
+namespace Experis_movie
+{
+    internal class mostPopularMovies
+    {
+        public static Dictionary<string, int> mostPopularMoviesPurchased(List<Users> users, List<Products> products)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (Users user in users)
+            {
+                foreach (Products movie in products)
+                {
+                    foreach (string value in user.UserPurchasedSplit)
+                    {
+                        if (value.Replace(" ", "").Equals(movie.ProductId.ToString()))
+                        {
+                            if (dict.ContainsKey(movie.ProductName))
+                            {
+                                dict[movie.ProductName] = dict[movie.ProductName] + 1;
+                            }
+                            else
+                            {
+                                dict.Add(movie.ProductName, 1);
+                            }
+                        }
+                    }
+                }
+            }
+            var sortedDict = from entry in dict orderby entry.Value descending select entry;
+            return dict = sortedDict.ToDictionary(pair => pair.Key, pair => pair.Value);
+        }
+
+        public static Dictionary<string, double> mostPopularMoviesByRating(List<Products> products)
+        {
+            Dictionary<string, double> dict = new Dictionary<string, double>();
+
+            foreach (Products product in products)
+            {
+                if (dict.ContainsKey(product.ProductName))
+                {
+                    dict[product.ProductName] = product.ProductRating;
+                }
+                else
+                {
+                    dict.Add(product.ProductName, product.ProductRating);
+                }
+            }
+            var sortedDict = from entry in dict orderby entry.Value descending select entry;
+            return dict = sortedDict.ToDictionary(pair => pair.Key, pair => pair.Value);
+        }
+    }
+}
+
